ff723827fab944c93ec6af28070913ece8870921
Hari Hara Sudhan S <hari19@outlook.in>
2023-02-18 21:32:39 +0530

    Bottom up BFS implemented

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -1,5 +1,5 @@
 #include "bfs.h"
-
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -98,6 +98,66 @@ void bfs_top_down(Graph graph, solution* sol) {
     }
 }
 
+
+
+// Take one step of "bottom-up" BFS.
+void bottom_up_step(
+    Graph g,
+    vertex_set* frontier,
+    vertex_set* new_frontier,
+    int* distances)
+{
+    for (Vertex v=0; v<g->num_nodes; v++) {
+        if(distances[v] == NOT_VISITED_MARKER) {
+            int start_edge = g->incoming_starts[v];
+            int end_edge = (v == g->num_nodes - 1)
+                           ? g->num_edges
+                           : g->incoming_starts[v + 1];
+                           
+            for(int j = start_edge; j<end_edge; j++) {
+                bool flag = false;
+                Vertex u = g->incoming_edges[j];
+                for (int i=0; i<frontier->count; i++) {
+                    int node = frontier->vertices[i];
+                    if(node == u) {
+                        distances[v] = distances[u] + 1;
+                        new_frontier->vertices[new_frontier->count++] = v;
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag) {
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
+    // for (int i=0; i<frontier->count; i++) {
+
+    //     int node = frontier->vertices[i];
+
+    //     int start_edge = g->outgoing_starts[node];
+    //     int end_edge = (node == g->num_nodes - 1)
+    //                        ? g->num_edges
+    //                        : g->outgoing_starts[node + 1];
+
+    //     // attempt to add all neighbors to the new frontier
+    //     for (int neighbor=start_edge; neighbor<end_edge; neighbor++) {
+    //         int outgoing = g->outgoing_edges[neighbor];
+
+    //         if (distances[outgoing] == NOT_VISITED_MARKER) {
+    //             distances[outgoing] = distances[node] + 1;
+    //             int index = new_frontier->count++;
+    //             new_frontier->vertices[index] = outgoing;
+    //         }
+    //     }
+    // }
+// }
+
+
 void bfs_bottom_up(Graph graph, solution* sol)
 {
     // You will need to implement the "bottom up" BFS here as
@@ -109,6 +169,43 @@ void bfs_bottom_up(Graph graph, solution* sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+
+    
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+
+    vertex_set* frontier = &list1;
+    vertex_set* new_frontier = &list2;
+
+    // initialize all nodes to NOT_VISITED
+    for (int i=0; i<graph->num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (frontier->count != 0) {
+
+#ifdef VERBOSE
+        double start_time = CycleTimer::currentSeconds();
+#endif
+
+        vertex_set_clear(new_frontier);
+
+        bottom_up_step(graph, frontier, new_frontier, sol->distances);
+
+#ifdef VERBOSE
+    double end_time = CycleTimer::currentSeconds();
+    printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+#endif
+        // swap pointers
+        vertex_set* tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+    }
 }
 
 void bfs_hybrid(Graph graph, solution* sol)
